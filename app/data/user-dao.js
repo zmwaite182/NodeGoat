61774d49f0695c3112236fbbc8836a92b607720a
Zane Waite <zmwaite182@stevenscollege.edu>
2019-02-26 15:18:41 -0500

    A2 - 1 Session Management

--- app/data/user-dao.js
+++ app/data/user-dao.js
@@ -65,7 +65,7 @@ function UserDAO(db) {
 
         // Helper function to compare passwords
         function comparePassword(fromDB, fromUser) {
-            return fromDB === fromUser;
+
 
             // Fix for A2-Broken Auth
             // compares decrypted password stored in this.addUser()
